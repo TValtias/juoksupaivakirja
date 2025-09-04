d0288a30085d22c083216921df0d42f698934409
TValtias <177140336+TValtias@users.noreply.github.com>
2025-09-04 11:14:53 +0300

    Added route framework to app.py

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return "Tähän tulee juoksupäiväkirja"
+
+@app.route("/login")
+def login():
+    return "Kirjautuminen tulossa pian!"
+
+@app.route("/paivakirja")
+def diary():
+    return "Päiväkirja tulossa pian!"
+
+@app.route("/kisat")
+def competitions():
+    return "Kisat tulossa pian!"
+
+@app.route("/kayttaja/<int:page_id>")
+def user_page(page_id):
+    return "Käyttäjien sivut tulossa pian!"
