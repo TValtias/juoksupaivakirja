883a1d00c19bda3ea733d88927361f0c091e5542
TValtias <177140336+TValtias@users.noreply.github.com>
2025-09-24 18:06:01 +0300

    images and information for competitions

---
+++ seed_kisat.sql
@@ -0,0 +1,21 @@
+INSERT INTO competitions (name, description, description2, banner_image, map_image)
+VALUES
+(
+    'Lenkkipolun 10km',
+    'Lenkkipolun rakastettu 10km juoksu on taas täällä.
+    Huijaa tiesi huipulletai kukista muut kunniakkaasti!
+    Nyt saa huijata!',
+    'Aika: 10.10.2025
+    Paikka: Hyvinaito puisto 1 A, Hämeenlinna',
+    'lenkkipolku_banner.png',
+    'lenkkipolku_kartta.png'
+),
+(
+    'Lenkkien yö',
+    'Nyt juostaan yhdessä!
+    Ketään ei jätetä yksin, kun valaisemme lenkkipolut valotikuilla.',
+    'Aika: 11.11.2025
+    Paikka: Toinen aitopaikka 14, Helsinki',
+    'lenkkiyo_banner.png',
+    'lenkkiyo_kartta.png'
+);
